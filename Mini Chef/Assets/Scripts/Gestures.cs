90243637ca55910e2d43aaf1ea48c1f1aa10618a
LuisMartinez1621 <73976465+LuisMartinez1621@users.noreply.github.com>
2020-11-10 11:14:05 -0800

    Objetos Movibles
    Se creó el "tablero" donde los modelos de los ingredientes estarán.
    Actualmente están representados por cubos y se pueden mover. Al soltarlos regresará a su posición inicial.

---
+++ Mini Chef/Assets/Scripts/Gestures.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Gestures : MonoBehaviour
+{
+    // Vector de la distancia del objeto
+    Vector3 dist;
+
+    // Vector de la posición inicial
+    Vector3 startPosition;
+
+    // Variable del asset
+    //public GameObject target;
+
+    // Variables que guardan las posiciones X y Y
+    float posX;
+    float posY;
+
+    private void Awake()
+    {
+        //startPosition = transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+    }
+
+    private void OnMouseUp()
+    {
+        Vector3 cosa = transform.parent.GetComponent<Transform>().position;
+        //transform.position = cosa; //transform.parent.GetComponent<Transform>().position;
+        transform.localPosition = new Vector3(0, 20f, 0);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curPos =
+         new Vector3(Input.mousePosition.x - posX,
+                     Input.mousePosition.y - posY, dist.z);
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+}
---
+++ Mini Chef/Assets/Scripts/Level1Manager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour
+{
+    // ImageTarget
+    public GameObject imageTarget;
+
+    // Cilindro
+    public GameObject cylinder;
+
+    // Cubo
+    public GameObject cube;
+
+    // Array de cilindros
+    GameObject[,] cylinders = new GameObject[3,3];
+
+    // Array de los ingredientes
+    GameObject[,] ingredients = new GameObject[3, 3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Scale and position
+
+        // Ciclo que genera las bases de los ingredientes
+        for (int x = 0; x < 3; x++)
+        {
+            for(int y = 0; y < 3; y++)
+            {
+                // Se crea una instancia de un objeto "Cylinder"
+                cylinders[x, y] = Instantiate(cylinder, imageTarget.transform.position, new Quaternion(0,0,0,0));
+
+                // Se asigna el ImageTarget como padre de la base
+                cylinders[x, y].transform.parent = imageTarget.transform;
+
+                // Se ajusta la escala
+                //cylinders[x, y].transform.localScale = scale;
+
+                // Se ajusta la posición
+                cylinders[x, y].transform.localPosition = new Vector3(x - 1, 0.1f, y - 1);
+            }
+        }
+
+        // Ingredientes!
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                // Se crea una instancia de un objeto "Cylinder"
+                ingredients[x, y] = Instantiate(cube, cylinders[x, y].GetComponent<Transform>().transform.position, new Quaternion(0, 0, 0, 0));
+
+                // Se asigna el ImageTarget como padre de la base
+                ingredients[x, y].transform.parent = cylinders[x, y].GetComponent<Transform>().transform;
+
+                // Se ajusta la escala
+                //ingredients[x, y].transform.localScale = scale;
+
+                // Se ajusta la posición
+                ingredients[x, y].transform.localPosition = new Vector3(0, 20f, 0);
+            }
+        }
+
+        /*for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                // Creando los cubos
+                ingredients[cubeCount] = Instantiate(cube, cylinders[x, y].GetComponent<Transform>().transform.position, new Quaternion(0, 0, 0, 0));
+                ingredients[cubeCount].transform.parent = cylinders[x, y].GetComponent<Transform>().transform;
+                ingredients[cubeCount].transform.localScale = scaleCube;
+                ingredients[cubeCount].transform.localPosition = new Vector3(0, 1f, 0);
+
+                cubeCount++;
+            }
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
