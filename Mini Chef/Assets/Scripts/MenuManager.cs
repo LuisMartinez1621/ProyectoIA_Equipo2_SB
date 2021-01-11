d10d6aa65ef9006d1c00f24c616c89d7647a777d
LuisMartinez1621 <73976465+LuisMartinez1621@users.noreply.github.com>
2021-01-11 12:02:44 -0800

    Estado antes de vacaciones
    Estado del programa antes de salir de vacaciones

---
+++ Mini Chef/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Pantallas
+    GameObject login;
+    GameObject register;
+
+    // Varaibles
+    GameObject backScreen;
+    GameObject currentScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Cosa para guardar la pantalla anterior
+    public void SetBackScreen(GameObject screen)
+    {
+        backScreen = screen;
+    }
+
+    public void SetCurrentScreen(GameObject screen)
+    {
+        currentScreen = screen;
+
+        backScreen.SetActive(false);
+        currentScreen.SetActive(true);
+    }
+}
---
+++ Mini Chef/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    float currentPos;
+    public float vel = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos -= vel;
+
+        if(currentPos <= -960)
+        {
+            currentPos = 0;
+            GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
+        }
+
+        GetComponent<RectTransform>().offsetMin = new Vector2(currentPos, 0);
+    }
+}
